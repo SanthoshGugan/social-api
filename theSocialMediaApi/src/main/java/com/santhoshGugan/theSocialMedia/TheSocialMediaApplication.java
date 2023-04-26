47e5d1905b3fbd0abcf651e9be10573b574493e2
santhosh <santhoshg1990@gmail.com>
2023-04-26 11:40:38 +0530

    spring boot setup complete

---
+++ theSocialMediaApi/src/main/java/com/santhoshGugan/theSocialMedia/TheSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.santhoshGugan.theSocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSocialMediaApplication.class, args);
+	}
+
+}
---
+++ theSocialMediaApi/src/test/java/com/santhoshGugan/theSocialMedia/TheSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santhoshGugan.theSocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
